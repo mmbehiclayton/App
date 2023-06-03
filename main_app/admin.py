85cd7daac72f6e715b1044a4e31f5bb6b7d313b9
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2023-06-03 11:01:44 +0300

    update admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -7,26 +7,61 @@ from .models import *
 class UserModel(UserAdmin):
     ordering = ('email',)
 
+
 admin.site.register(Branch)
 admin.site.register(ClassCategory)
-admin.site.register(CategoryTargetScore)
 
-admin.site.register(Classes)
-admin.site.register(Stream)
-admin.site.register(CustomUser, UserModel)
+@admin.register(CategoryTargetScore)
+class CategoryTargetScore(admin.ModelAdmin):
+    list_display = ('class_category','term','target_score')
+    list_filter = ('class_category','term')
+
+# Custom ModelAdmin for Classes
+class ClassesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'branch', 'created_at', 'updated_at')
+    list_filter = ('category', 'branch', 'created_at', 'updated_at')
+
+admin.site.register(Classes, ClassesAdmin)
+
+# Custom ModelAdmin for Stream
+class StreamAdmin(admin.ModelAdmin):
+    list_display = ('class_id', 'name', 'created_at', 'updated_at')
+    list_filter = ('class_id', 'created_at', 'updated_at')
+
+admin.site.register(Stream, StreamAdmin)
+
+#register custom user
+@admin.register(CustomUser)
+class CustomUser(admin.ModelAdmin):
+    list_display = ('email', 'user_type', 'gender', 'created_at', 'updated_at')
+    list_filter = ('user_type', 'gender', 'created_at', 'updated_at')
 
 #Register Staff on Admin Site
 admin.site.register(Staff)
 
-admin.site.register(Subject)
+# Custom ModelAdmin for Subject
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name','created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
 
-admin.site.register(Session)
-admin.site.register(SessionTerm)
+admin.site.register(Subject, SubjectAdmin)
 
-#Register Exam  on the Admin Site
-@admin.register(Exam)
-class Exam(admin.ModelAdmin):
-    list_display = ['exam_id','session','term','name','target_marks']
+@admin.register(Session)
+class Session(admin.ModelAdmin):
+    list_display = ('name', 'start_year', 'end_year')
+    list_filter = ('name', 'start_year', 'end_year')
+
+@admin.register(SessionTerm)
+class SessionTerm(admin.ModelAdmin):
+    list_display = ('term', 'session', 'name')
+    list_filter = ('term', 'session')
+
+# Custom ModelAdmin for Exam
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'exam_date', 'target_marks', 'session', 'term')
+    list_filter = ('session', 'term')
+
+admin.site.register(Exam, ExamAdmin)
 
 #Register Exam Result on the Admin Site
 @admin.register(ExamMeanResult)
